9f40a8153ccd1b1be2b6d9c6d120bcdfb99d6eb0
developerputul <putulsentongchangya7@gmail.com>
2024-07-16 23:43:17 +0600

    Admin Dashboard Manage Category Add Section Updated

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -12,4 +12,9 @@ class CategoryController extends Controller
         $category = Category::latest()->get();
         return view('admin.backend.category.all_category',compact('category'));
     } // End Method
+
+    public function AddCategory(){
+
+        return view('admin.backend.category.add_category');
+    } // End Method
 }
