d054e201e64eba514d29f49df645b8edf9e6e6c4
developerputul <putulsentongchangya7@gmail.com>
2024-08-02 13:24:44 +0600

    Frontend Course Payment Section Updated

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use App\Models\Coupon;
+use App\Models\Payment;
+use App\Models\Order;
 use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
@@ -213,9 +215,74 @@ class CartController extends Controller
                 'alert-type' => 'error'
             );
             return redirect()->route('login')->with($notification); 
-
         }
+    } // End Method
 
+    public function Payment(Request $request){
+
+        if (Session::has('coupon')) {
+            $total_amount = Session::get('coupon')['total_amount'];
+         }else {
+             $total_amount = round(Cart::total());
+         }
+ 
+         // Cerate a new Payment Record 
+ 
+         $data = new Payment();
+         $data->name = $request->name;
+         $data->email = $request->email;
+         $data->phone = $request->phone;
+         $data->address = $request->address;
+         $data->cash_delivery = $request->cash_delivery;
+         $data->total_amount = $total_amount;
+         $data->payment_type = 'Direct Payment';
+ 
+         $data->invoice_no = 'EOS' . mt_rand(10000000, 99999999);
+         $data->order_date = Carbon::now()->format('d F Y');
+         $data->order_month = Carbon::now()->format('F');
+         $data->order_year = Carbon::now()->format('Y');
+         $data->status = 'pending';
+         $data->created_at = Carbon::now(); 
+         $data->save();
+
+         foreach ($request->course_title as $key => $course_title) {
+            
+            $existingOrder = Order::where('user_id',Auth::user()->id)
+            ->where('course_id',$request->course_id[$key])->first();
+
+            if ($existingOrder) {
+               
+                $notification = array(
+                    'message' => 'You have already enrolled in this course',
+                    'alert-type' => 'error'
+                );
+                return redirect()->back()->with($notification); 
+            } //end if 
+
+            $order = new Order();
+            $order->payment_id = $data->id;
+            $order->user_id = Auth::user()->id;
+            $order->course_id = $request->course_id[$key];
+            $order->instructor_id = $request->instructor_id[$key];
+            $order->course_title = $course_title;
+            $order->price = $request->price[$key];
+            $order->save();
+
+         } //end foreach
+
+         $request->session()->forget('cart');
+
+         if ($request->cash_delivery == 'stripe') {
+            echo "stripe";
+         }else{
+
+             $notification = array(
+                 'message' => 'Cash Payment Submit Successfully',
+                 'alert-type' => 'success'
+             );
+             return redirect()->route('index')->with($notification); 
+
+         } 
 
     } // End Method
 }
--- resources/views/frontend/checkout/checkout_view.blade.php
+++ resources/views/frontend/checkout/checkout_view.blade.php
@@ -36,9 +36,9 @@
                     <h3 class="card-title fs-22 pb-3">Billing Details</h3>
                     <div class="divider"><span></span></div>
 
+        <form method="post" class="row" action="{{ route('payment') }}" enctype="multipart/form-data">
+            @csrf
 
-
-        <form method="post" class="row">
             <div class="input-box col-lg-6">
                 <label class="label-text">First Name</label>
                 <div class="form-group">
@@ -66,7 +66,7 @@
                     <input id="phone" class="form-control form--control" type="phone" name="phone" value="{{ Auth::user()->phone }}">
                 </div>
             </div><!-- end input-box -->
-        </form>
+        
     </div><!-- end card-body -->
 </div><!-- end card -->
 
@@ -103,6 +103,12 @@
 
                 @foreach ($carts as $item)
 
+                <input type="hidden" name="slug[]" value="{{ $item->options->slug }}">
+                <input type="hidden" name="course_id[]" value="{{ $item->id }}">
+                <input type="hidden" name="course_title[]" value="{{ $item->name }}">
+                <input type="hidden" name="price[]" value="{{ $item->price }}">
+                <input type="hidden" name="instructor_id[]" value="{{ $item->options->instructor }}">
+
                 <div class="media media-card border-bottom border-bottom-gray pb-3 mb-3">
                     <a href="{{ url('course/details/'.$item->id.'/'.$item->options->slug) }}" class="media-img">
                         <img src="{{ asset($item->options->image) }}" alt="Cart image">
@@ -124,7 +130,6 @@
                         <div class="divider"><span></span></div>
 
                         @if (Session::has('coupon'))
-
                         <ul class="generic-list-item generic-list-item-flash fs-15">
 
                             <li class="d-flex align-items-center justify-content-between font-weight-semi-bold">
@@ -147,31 +152,32 @@
                             </li>
                         </ul>
 
+                        <input type="hidden" name="total" value="{{ $cartTotal }}">
+
                         @else
-
                         <ul class="generic-list-item generic-list-item-flash fs-15">
-
                             <li class="d-flex align-items-center justify-content-between font-weight-bold">
                                 <span class="text-black">Total:</span>
                                 <span>${{ $cartTotal }}</span>
                             </li>
-
-                        </ul>
-                            
+                            <input type="hidden" name="total" value="{{ $cartTotal }}">
+                        </ul>  
                         @endif
-                      
-
 
                         <div class="btn-box border-top border-top-gray pt-3">
                             <p class="fs-14 lh-22 mb-2">Aduca is required by law to collect applicable transaction taxes for purchases made in certain tax jurisdictions.</p>
                             <p class="fs-14 lh-22 mb-3">By completing your purchase you agree to these <a href="#" class="text-color hover-underline">Terms of Service.</a></p>
-                            <a href="checkout.html" class="btn theme-btn w-100">Proceed <i class="la la-arrow-right icon ml-1"></i></a>
+
+                            <button type="submit" class="btn theme-btn w-100">Proceed <i class="la la-arrow-right icon ml-1"></i></button>
+                            
                         </div>
                     </div><!-- end card-body -->
                 </div><!-- end card -->
             </div><!-- end col-lg-5 -->
         </div><!-- end row -->
     </div><!-- end container -->
+
+</form>
 </section>
 <!-- ================================
        END CONTACT AREA
