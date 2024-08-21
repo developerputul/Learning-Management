0e7bd8dd91209a6d579b1ffd4b8ff06e5796d331
developerputul <putulsentongchangya7@gmail.com>
2024-08-21 12:35:09 +0600

    Admin Dashboard Import & Export Section Updated

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
          */
         // Intervention\Image\ImageServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -186,6 +187,8 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
         // 'Image' => Intervention\Image\Facades\Image::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+
     ])->toArray(),
 
 ];
