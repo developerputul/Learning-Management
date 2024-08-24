eec7cbe9844cfc42fdbb44babe1c126d39fd78ea
developerputul <putulsentongchangya7@gmail.com>
2024-08-25 00:39:22 +0600

    Frontend Live Chat Option Updated

---
+++ resources/js/components/SendMessage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+       <!-- Button trigger modal -->
+<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+  Live Chat
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Chat With</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <form action="">
+            <div class="modal-body">
+            <textarea class="form-control" rows="3" placeholder="Type Your Message"></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Send Message</button>
+            </div>
+    </form>
+
+    </div>
+  </div>
+</div>
+    </div>
+</template>
--- resources/views/frontend/instructor/instructor_details.blade.php
+++ resources/views/frontend/instructor/instructor_details.blade.php
@@ -17,6 +17,9 @@
                     <p class="lh-18">{{ $instructor->email }}</p>
                 </div>
             </div><!-- end media -->
+
+           
+
             <ul class="social-icons social-icons-styled social--icons-styled">
                 <li><a href="#"><i class="la la-facebook"></i></a></li>
                 <li><a href="#"><i class="la la-twitter"></i></a></li>
@@ -199,6 +202,15 @@
     <div class="bg-gray py-5">
         <div class="container">
             <ul class="nav nav-tabs generic-tab justify-content-center" id="myTab" role="tablist">
+
+                <li class="nav-item">
+                 <!-- Vue start-->
+                <div id="app">
+                    <send-message> </send-message>
+                </div>
+            <!-- Vue end -->
+            </li>
+
                 <li class="nav-item">
                     <a class="nav-link active" id="about-me-tab" data-toggle="tab" href="#about-me" role="tab" aria-controls="about-me" aria-selected="false">
                         About Me
